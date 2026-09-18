867e3d8367170c3a7d324871182f0d2a2eaaccd2
KrishDevCrafting <krishkumer150@gmail.com>
2026-09-18 23:16:26 +0530

    completed

---
+++ Backend/imp.jsx
@@ -0,0 +1,4 @@
+// PORT=5000
+// MONGODB_URI=mongodb+srv://krishkapoor:db.krish2004@cluster0.vwsknd0.mongodb.net/?appName=Cluster0
+// JWT_SECRET=shopkart_super_secret_key_2026
+// NODE_ENV=development
